15fd8a025af4e58ea25d3600ebcffaacc0e5617f
rgerum <14153051+rgerum@users.noreply.github.com>
2022-09-14 16:18:11 -0400

    fixed indent

--- tests/test_code_generation.py
+++ tests/test_code_generation.py
@@ -15,35 +15,35 @@ class TestFits(unittest.TestCase):
         with self.filename.open("w") as fp:
             print("write tmp file")
             fp.write("""
-    import matplotlib.pyplot as plt
-    import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-    # now import pylustrator
-    import pylustrator
+# now import pylustrator
+import pylustrator
 
-    # activate pylustrator
-    pylustrator.start()
+# activate pylustrator
+pylustrator.start()
 
-    # build plots as you normally would
-    np.random.seed(1)
-    t = np.arange(0.0, 2, 0.001)
-    y = 2 * np.sin(np.pi * t)
-    a, b = np.random.normal(loc=(5., 3.), scale=(2., 4.), size=(100,2)).T
-    b += a
+# build plots as you normally would
+np.random.seed(1)
+t = np.arange(0.0, 2, 0.001)
+y = 2 * np.sin(np.pi * t)
+a, b = np.random.normal(loc=(5., 3.), scale=(2., 4.), size=(100,2)).T
+b += a
 
-    plt.figure(1)
-    plt.subplot(131)
-    plt.plot(t, y)
+plt.figure(1)
+plt.subplot(131)
+plt.plot(t, y)
 
-    plt.subplot(132)
-    plt.plot(a, b, "o")
+plt.subplot(132)
+plt.plot(a, b, "o")
 
-    plt.subplot(133)
-    plt.bar(0, np.mean(a))
-    plt.bar(1, np.mean(b))
+plt.subplot(133)
+plt.bar(0, np.mean(a))
+plt.bar(1, np.mean(b))
 
-    # show the plot in a pylustrator window
-    plt.show(hide_window=True)
+# show the plot in a pylustrator window
+plt.show(hide_window=True)
     """)
 
     def tearDown(self):
