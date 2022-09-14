1173670329d05a37bbb7b6880000045ea5d4019a
rgerum <14153051+rgerum@users.noreply.github.com>
2022-09-14 16:10:30 -0400

    import pylustrator

--- test_pid.py
+++ test_pid.py
@@ -1,6 +1,6 @@
 #from PySide2.QtWidgets import qApp
 
 
-def test_pid(qtbot):
+def test_pid():
     print("yes")
     #print(f"My pid: {qApp.applicationPid()}")
--- tests/test_code_generation.py
+++ tests/test_code_generation.py
@@ -1,4 +1,13 @@
 import unittest
+import numpy as np
+
+import sys
+import mock
+import os
+from pathlib import Path
+import matplotlib.pyplot as plt
+from matplotlib import _pylab_helpers
+import pylustrator
 
 class TestFits(unittest.TestCase):
     def test_fitCamParametersFromObjects(self):
