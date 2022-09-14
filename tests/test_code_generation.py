88645ead2bd94c554962410462470dc5c6742278
rgerum <14153051+rgerum@users.noreply.github.com>
2022-09-14 15:54:06 -0400

    update2

--- tests/test_code_generation.py
+++ tests/test_code_generation.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Pylustrator. If not, see <http://www.gnu.org/licenses/>
 import sys
+sys.argv = ["-platform", "minimal"]
 print("start")
 print(sys.argv)
 import matplotlib
